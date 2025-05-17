9e435dc01071a7ca94504eb412fc6b4ce7485ac8
Ramón Márquez <ramonmarquez3@gmail.com>
2025-05-17 21:25:35 +0200

    Primera versión AUX dApp

---
+++ src/App.js
@@ -0,0 +1,8 @@
+
+import { useEffect, useState } from "react";
+import { ethers } from "ethers";
+import toast, { Toaster } from "react-hot-toast";
+// ... (resto de AuxApp pegado antes)
+export default function AuxApp() {
+  return <div className="text-white p-6">AUX DApp restaurada</div>;
+}
