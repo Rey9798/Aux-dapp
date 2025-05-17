0ff77b6e56e983e2af9de29c19443db9765eb300
Rey9798 <ramonmarquez3@gmail.com>
2025-05-18 01:41:33 +0200

    deploy: AUX DApp desde cero

--- src/App.js
+++
@@ -1,8 +0,0 @@
-
-import { useEffect, useState } from "react";
-import { ethers } from "ethers";
-import toast, { Toaster } from "react-hot-toast";
-// ... (resto de AuxApp pegado antes)
-export default function AuxApp() {
-  return <div className="text-white p-6">AUX DApp restaurada</div>;
-}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
-module.exports = {
-  content: ["./src/**/*.{js,jsx}"],
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
