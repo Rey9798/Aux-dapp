4c5ca2a05b49b143fad8ae075c2c06521f8c6fea
Rey9798 <ramonmarquez3@gmail.com>
2025-05-17 21:50:33 +0200

    fix: añadir index.html y render raíz

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import AuxApp from './App';
 import './index.css';
+import AuxApp from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
